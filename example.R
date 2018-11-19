ba88f5a5037c6414e7e1b9cd82418d43749401f6
Robin Lovelace <rob00x@gmail.com>
2018-11-19 18:05:48 +0000

    Add example file

---
+++ example.R
@@ -0,0 +1,25 @@
+######### Example R script #########
+###                              ###
+###                              ###
+### Lex Comber                   ###
+### November 2018                ###
+
+# load packages
+library(GISTools)
+library(rgdal)
+library(raster)
+library(reshape2)
+library(tidyverse)
+library(OpenStreetMap)
+library(grid)
+library(GWmodel)
+library(tmap)
+library(nlme)
+library(repmis)
+library(rgdal)
+library(lmtest)
+library(sandwich)
+library(rgdal)
+library(sf)
+
+# Part 1 load data
